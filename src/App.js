a9bb68abeb8128a000b3b5b6d764068d788a8b2f
YunNamwon <109219153+YunNamwon@users.noreply.github.com>
2022-10-10 21:29:49 +0900

    v2

--- src/App.js
+++ src/App.js
@@ -1,29 +1,58 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 import Customer from './components/Customer';
 
-const customer = {
+
+const customers = [
+  {
   'id' : 1,
-  'image': 'https://placeimg.com/64/64/any',
+  'image': 'https://placeimg.com/64/64/1',
   'name' : '윤남원',
   'birthday' : '991117',
   'gender' : '남자',
   'job' : '대학생',
+  },
+  {
+    'id' : 2,
+    'image': 'https://placeimg.com/64/64/2',
+    'name' : '홍길동',
+    'birthday' : '111111',
+    'gender' : '남자',
+    'job' : '학생',
+  },
+  {
+    'id' : 3,
+    'image': 'https://placeimg.com/64/64/3',
+    'name' : '김철수',
+    'birthday' : '222222',
+    'gender' : '남자',
+    'job' : '대학생',
+  }
+]
 
-}
 
-
-function App() {
+class App extends Component {
+  render(){
   return (
-    <Customer
-     id = {customer.id}
-     image = {customer.image}
-     name={customer.name}
-     birthday={customer.birthday}
-     gender={customer.gender}
-     job={customer.job}
-    /> 
-  );
+    <div>
+      {
+        customers.map(c => {
+          return( 
+          <Customer
+          key={c.id} // key 값 
+          id={c.id}
+          image={c.image}
+          name={c.name}
+          birthday={c.birthday} 
+          gender={c.gender}
+          job={c.job}
+          />
+          );
+        })
+      }
+    </div>
+   );
+   }
 }
 
 export default App;
